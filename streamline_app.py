495cc619300816087b829a535fcde027248e771b
Pavel Hanys <112647671+pavelhanys@users.noreply.github.com>
2022-09-01 19:53:17 +0200

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents new heathy dinner')
+streamlit.header('header')
+streamlit.text('text line 1')
+streamlit.text('text line 2')
+streamlit.text('text line 3')
